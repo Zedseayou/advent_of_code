9e57e325d9cfcc5889d634a0f1c2dfe7a5613b51
Calum You <calum@uber.com>
2021-12-02 01:35:22 -0500

    switch to cumsum

--- 2021/day_2_dive.R
+++ 2021/day_2_dive.R
@@ -20,25 +20,14 @@ q3(input_2)
 
 
 q4 <- function(input) {
-  change_aim <- function(aim, direction, distance) {
-    if (direction == "down") {
-      aim <- aim + distance
-    } else if (direction == "up"){
-      aim <- aim - distance
-    }
-    aim
-  }
-  aim <- accumulate2(
-    .x = input$direction,
-    .y = input$distance,
-    .f = change_aim,
-    .init = 0
-  ) %>%
-    as.integer()
-
   position <- input %>%
     mutate(
-      aim = aim[-1],
+      da = case_when(
+        direction == "up" ~ - distance,
+        direction == "down" ~ distance,
+        TRUE ~ 0
+      ),
+      aim = cumsum(da),
       dx = if_else(direction == "forward", distance, 0),
       dy = if_else(direction == "forward", distance * aim, 0)
     ) %>%
