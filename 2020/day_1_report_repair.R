c0650867de452b36cfcd22640fc5fd6c204dc5c6
Calum You <calum@uber.com>
2021-12-03 18:44:48 -0500

    2020 day 1 and 2

---
+++ 2020/day_1_report_repair.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+input_1 <- read_csv("2020/data/input_1.txt", col_names = FALSE)
+
+q1a <- function(input) {
+  combos <- combn(x = input$X1, m = 2)
+  index <- which(combos[1,] + combos[2,] == 2020)
+  prod(combos[, index])
+}
+
+q1a(input_1)
+
+q1b <- function(input) {
+  combos <- combn(x = input$X1, m = 3)
+  index <- which(colSums(combos) == 2020)
+  prod(combos[, index])
+}
+
+q1b(input_1)
---
+++ 2020/day_2_password_philosophy.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+
+input_2 <- read_delim(
+  file = "2020/data/input_2.txt",
+  delim = " ",
+  col_names = c("number", "letter", "password")
+)
+
+
+q2a <- function(input) {
+  clean <- input %>%
+    separate(number, c("min", "max"), sep = "-") %>%
+    mutate(
+      min = as.integer(min),
+      max = as.integer(max),
+      letter = str_remove_all(letter, ":")
+    )
+
+  validated <- clean %>%
+    mutate(letters = map(password, ~ str_split(.x, "")[[1]])) %>%
+    unnest_longer(letters) %>%
+    count(min, max, letter, password, letters) %>%
+    ungroup() %>%
+    filter(letter == letters) %>% # if letter doesn't appear, it's invalid (min >= 1)
+    mutate(valid = n >= min & n <= max)
+  sum(validated$valid)
+}
+
+q2a(input_2)
+
+q2b <- function(input) {
+  clean <- input %>%
+    separate(number, c("pos1", "pos2"), sep = "-") %>%
+    mutate(
+      pos1 = as.integer(pos1),
+      pos2 = as.integer(pos2),
+      letter = str_remove_all(letter, ":")
+    )
+
+  validated <- clean %>%
+    mutate(letters = map(password, ~ str_split(.x, "")[[1]])) %>%
+    unnest_longer(letters, indices_to = "position") %>%
+    filter(pos1 == position | pos2 == position) %>%
+    mutate(match = letter == letters) %>%
+    group_by(password) %>%
+    filter(sum(match) == 1)
+  sum(validated$match)
+}
+
+q2b(input_2)
