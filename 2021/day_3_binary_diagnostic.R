25bee23e47927aa104508fb10e228a95f72ea59a
Calum You <calum@uber.com>
2021-12-03 14:25:22 -0500

    day 3 with reduce

--- 2021/day_3_binary_diagnostic.R
+++ 2021/day_3_binary_diagnostic.R
@@ -47,7 +47,7 @@ q3a <- function(input) {
 
 q3a(input_3)
 
-support_rating <- function(input, support = c("o2", "co2")) {
+support_rating <- function(input, bit_num, support = c("o2", "co2")) {
   index_to_keep <- function(bit_vals, support) {
     mlcv <- most_least_common(bit_vals)
     if (support == "o2") {
@@ -66,25 +66,26 @@ support_rating <- function(input, support = c("o2", "co2")) {
     bit_vals == keep_digit
   }
 
-  working <- input
-  for (bit_num in seq_along(working)) {
-    bit_vals <- working[[bit_num]]
-    working <- working[index_to_keep(bit_vals, support), ]
-    if (nrow(working) == 1) {
-      break
-    }
-  }
-  working %>%
-    str_c(collapse = "") %>%
-    bin_to_int()
-}
+  bit_vals <- input[[bit_num]]
+  out <- filter(input, index_to_keep(bit_vals, support))
 
-support_rating(test_3, "o2")
-support_rating(test_3, "co2")
+  if (nrow(out) == 1) {
+    out <- out %>%
+      str_c(collapse = "") %>%
+      bin_to_int()
+    done(out) # exit reduce if we are finished
+  } else {
+    out
+  }
+
+}
+support_rating(test_3, 1,  "o2")
+support_rating(test_3, 1, "co2")
+reduce(1:5, support_rating, support = "o2", .init = test_3)
 
 q3b <- function(input) {
-  o2_rating <- support_rating(input, "o2")
-  co2_rating <- support_rating(input, "co2")
+  o2_rating <- reduce(seq_along(input), support_rating, support = "o2", .init = input)
+  co2_rating <- reduce(seq_along(input), support_rating, support = "co2", .init = input)
   o2_rating * co2_rating
 }
 
