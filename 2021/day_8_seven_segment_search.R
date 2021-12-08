13d96f2968c62a2a32e48939981a2e8c63e00489
Calum You <calum@uber.com>
2021-12-08 06:00:14 +0000

    day 8 solution

---
+++ 2021/day_8_seven_segment_search.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+
+parse_input <- function(input) {
+  input %>%
+    enframe(name = NULL) %>%
+    separate(value, c("input", "output"), sep = " \\| ") %>%
+    mutate(across(.fns = ~ str_split(.x, " ")))
+}
+
+test_8 <- read_lines("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce") %>% parse_input
+
+input_8 <- read_lines("2021/data/input_8.txt") %>% parse_input
+
+uniques <- input_8 %>%
+  pull(output)
+  flatten_chr() %>%
+  str_length()
+
+sum(uniques %in% c(2, 3, 4, 7))
+
+tibble(
+  num = 0L:9L,
+  n_segments = c(6, 2, 5, 5, 4, 5, 6, 3, 7, 6)
+)
+
+
+
+decode_segments <- function(input) {
+  letters <- str_split(input, "")
+
+  n <- lengths(letters)
+  n
+  one <- letters[[which(n == 2)]]
+  four <- letters[[which(n == 4)]]
+  seven <- letters[[which(n == 3)]]
+  eight <- letters[[which(n == 7)]]
+  n6 <- letters[n == 6]
+  n5 <- letters[n == 5]
+
+  three_index <- map_lgl(n5, ~ all(seven %in% .x))
+  three <- n5[three_index][[1]]
+  n5 <- n5[-which(three_index)]
+
+  six_index <- map_lgl(n6, ~ !all(one %in% .x))
+  six <- n6[six_index][[1]]
+  n6 <- n6[-which(six_index)]
+
+  five_index <- map_lgl(n5, ~ all(.x %in% six))
+  five <- n5[five_index][[1]]
+  two <- n5[!five_index][[1]]
+
+  zero_index <- map_lgl(n6, ~ length(intersect(.x, four)) == 3)
+  zero <- n6[zero_index][[1]]
+  nine <- n6[!zero_index][[1]]
+
+  codes <- list(zero, one, two, three, four, five, six, seven, eight, nine) %>%
+    map_chr(~ .x %>% sort() %>% str_c(collapse = ""))
+
+  0:9 %>%
+    as.character() %>%
+    set_names(codes)
+}
+
+decode_segments(test_8$input[[1]])
+
+calculate_output <- function(input, output) {
+  codes <- decode_segments(input)
+
+  out_indexes <- output %>%
+    str_split("") %>%
+    map_chr(~ .x %>% sort() %>% str_c(collapse = ""))
+
+  codes[out_indexes] %>%
+    str_c(collapse = "") %>%
+    as.integer()
+}
+
+calculate_output(test_8$input[[1]], test_8$output[[1]])
+
+q8b <- function(input) {
+  sum(pmap_int(input, calculate_output))
+}
+
+q8b(test_8[2:11, ])
+q8b(input_8)
