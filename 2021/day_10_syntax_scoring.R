b8f1a0a4adfa64e0293fa0c036a068a49c7b62d5
Calum You <calum@uber.com>
2021-12-10 11:09:13 +0000

    day 10 solutions

--- 2021/day_10_syntax_scoring.R
+++ 2021/day_10_syntax_scoring.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-input_10 <- read_lines("2021/data//input_10.txt")
+input_10 <- read_lines("2021/data//input_10.txt") %>% str_split("")
 
 test_10 <- read_lines("[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
@@ -13,101 +13,86 @@ test_10 <- read_lines("[({(<(())[]>[[{[]{<()<>>
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]") %>% str_split("")
 
-scores <- tibble(char = c(")", "]", "}", ">"), score = c(3, 57, 1197, 25137))
+check_line <- function(line, debug = FALSE) {
+  open <- c("(", "[", "{", "<")
+  close <- c(")", "]", "}", ">")
+  names(close) <- open
+  expected <- character()
 
-cor <- str_split("{([(<{}[<>[]}>{[]{[(<()>", "")[[1]]
-# at each close, sum between the opener and the close for the other brackets
-
-check_line <- function(line) {
-  brackets = tibble(
-    char = c("(", ")", "[", "]", "{", "}", "<", ">"),
-    is_close = rep(c(FALSE, TRUE), times = 4),
-    b1 = c(1, -1, rep(0, 6)),
-    b2 = c(0, 0, 1, -1, rep(0, 4)),
-    b3 = c(rep(0, 4), 1, -1, 0, 0),
-    b4 = c(rep(0, 6), 1, -1)
-  )
-  close_open <- c(")" = "(", "]" = "[", "}" = "{", ">" = "<")
-
-  line_tbl <- line %>%
-    enframe(name = NULL, value = "char") %>%
-    left_join(brackets, by = "char")
-
-  handle_chunk <- function(line_tbl) {
-    chunk_end <- which.max(line_tbl$is_close)
-    open_indexes <- which(line_tbl$char == close_open[line_tbl$char[chunk_end]])
-    if (length(open_indexes) == 0) {
-      return(line_tbl$char[chunk_end])
+  for (c in line) {
+    if (debug) {
+      Sys.sleep(0.5)
+      msg <- str_glue("Expecting {expected[1]}, found {c}")
+      message(msg)
     }
-    chunk_start <- max(open_indexes[open_indexes < chunk_end])
-    # print(c(chunk_start, chunk_end))
-    chunk <- slice(line_tbl, chunk_start:chunk_end)
-    b_sums <- map_dbl(c("b1", "b2", "b3", "b4"), ~ sum(chunk[[.x]]))
-    if(all(b_sums == 0)) {
-      out <- slice(line_tbl, -(chunk_start:chunk_end))
-    } else {
-      out <- line_tbl$char[chunk_end]
+
+    if (c %in% open) {
+      expected <- c(unname(close[c]), expected)
+    } else if (c %in% close) {
+      if (c == expected[1]) {
+        expected <- expected[-1]
+      } else if (c != expected[1]) {
+        return(c)
+      }
     }
-    out
   }
-  working <- line_tbl
-  while (inherits(working, "tbl")) {
-    # Sys.sleep(0.5)
-    working <- handle_chunk(working)
-  }
-  working
+  return("not corrupted")
 }
-check_line(cor)
-check_line(test_10[[3]])
-
-map(test_10, check_line)
+check_line(test_10[[1]], debug = TRUE)
+check_line(test_10[[3]], debug = TRUE)
 
 
-check_line <- function(line) {
-  brackets = tibble(
-    char = c("(", ")", "[", "]", "{", "}", "<", ">"),
-    is_close = rep(c(FALSE, TRUE), times = 4),
-    b1 = c(1, -1, rep(0, 6)),
-    b2 = c(0, 0, 1, -1, rep(0, 4)),
-    b3 = c(rep(0, 4), 1, -1, 0, 0),
-    b4 = c(rep(0, 6), 1, -1)
-  )
-  close_open <- c(")" = "(", "]" = "[", "}" = "{", ">" = "<")
+q10a <- function(input, debug = FALSE) {
+  scores <- tibble(char = c(")", "]", "}", ">"), score = c(3, 57, 1197, 25137))
+  line_chars <- map_chr(input, check_line, debug = debug)
 
-  line_tbl <- line %>%
+  line_chars %>%
     enframe(name = NULL, value = "char") %>%
-    left_join(brackets, by = "char")
-
-
-  is_legal <- function(char_index, line_tbl) {
-    if (line_tbl$is_close[char_index]) {
-      open_indexes <- which(line_tbl$char == close_open[line_tbl$char[char_index]])
-      chunk_start <- max(open_indexes[open_indexes < char_index])
-
-      b_sums <- map_dbl(
-        .x = c("b1", "b2", "b3", "b4"),
-        .f = ~ sum(line_tbl[[.x]][chunk_start:char_index])
-      )
-      # legal = all(b_sums == 0)
-      legal = b_sums
-    } else {
-      legal = TRUE
-    }
-    legal
-  }
-
-  line_tbl %>%
-    mutate(is_legal = map(1:nrow(line_tbl), is_legal, line_tbl = line_tbl))
-
-}
-check_line(cor) %>% view()
-check_line(test_10[[1]]) %>% view()
-
-q10a <- function(input) {
-  pats <- c("\\(", "\\)", "\\[", "\\]", "\\{", "\\}", "<", ">")
-  map(pats, ~ str_count(input, .x))
+    filter(char != "not corrupted") %>%
+    left_join(scores, by = "char") %>%
+    pull(score) %>%
+    sum()
 }
 
 q10a(test_10)
+q10a(input_10, debug = FALSE)
 
-map(c("(", ")", "[", "]", "{", "}", "<", ">"), ~ str_count(test_10, pattern = .x))
+complete_line <- function(line, debug = FALSE) {
+  open <- c("(", "[", "{", "<")
+  close <- c(")", "]", "}", ">")
+  names(close) <- open
+  expected <- character()
+
+  for (c in line) {
+    if (debug) {
+      Sys.sleep(0.5)
+      msg <- str_glue("Stack looks like {str_c(expected, collapse = ', ')}")
+      message(msg)
+    }
+
+    if (c %in% open) {
+      expected <- c(unname(close[c]), expected)
+    } else if (c == expected[1]) {
+      expected <- expected[-1]
+    }
+  }
+  expected
+}
+
+complete_line(test_10[[1]], debug = FALSE)
+
+q10b <- function(input, debug = FALSE) {
+  is_corrupt <- map_chr(input, check_line, debug = debug)
+  valid_lines <- input[is_corrupt == "not corrupted"]
+
+  scores <- tibble(char = c(")", "]", "}", ">"), score = 1:4)
+
+  completions <- map(valid_lines, complete_line)
+
+  completions %>%
+    enframe(name = NULL, value = "chars") %>%
+    rowid_to_column(var = "line_id") %>%
+    unnest(chars) %>%
+    left_join
+
+}
