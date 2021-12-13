9f99de1e104095ef1f6dfa150ec944aa005eb3e9
Calum You <calum@uber.com>
2021-12-13 00:08:44 +0000

    day 12 refactor

--- 2021/day_12_passage_pathing.R
+++ 2021/day_12_passage_pathing.R
@@ -3,9 +3,7 @@ library(tidyverse)
 parse_input <- function(input) {
   edges <- read_delim(input, delim = "-", col_names = c("from", "to"))
 
-  reversed <- edges %>%
-    filter(from != "start", to != "end") %>%
-    select(from = to, to = from)
+  reversed <- select(edges, from = to, to = from)
 
   bind_rows(edges, reversed)
 }
@@ -17,8 +15,6 @@ A-b
 b-d
 A-end
 b-end")
-
-test_12a
 
 test_12b <- parse_input("dc-end
 HN-start
@@ -52,79 +48,94 @@ start-RW")
 
 input_12 <- parse_input("2021/data/input_12.txt")
 
-# until reached the end:
-# start with a node
-# choose a next node to go to, from options in branches\
-# if it's small, keep track
-# once at the end:
-# compare to existing paths found already
-# if path already found, retreat back to last node with other options
-# do this by going back one node, and checking if there are other options
-# compare set of options at that node to matching paths()
-# if there is a available node not already in a matching path, take it
-# if not, retreat one more node
-
 # For edge list, current node, and existing path: show allowed next nodes
 node_options <- function(input, node, path) {
   small <- str_subset(path, "^[a-z]+$")
-  input %>%
-    filter(from == node, !(to %in% small)) %>%
-    pull(to)
+  if (node == "end") {
+    out <- character()
+  } else {
+    out <- input$to[input$from == node & !(input$to %in% small)]
+  }
+  out
 }
 
 node_options(test_12a, "start", "start")
 node_options(test_12a, "d", c("d", "b", "start"))
 node_options(test_12a, "A", c("A", "b", "start"))
 
-# Find a valid forward path from a point
-find_path <- function(input, node = "start", path = "start") {
-  current <- node
-  while (current != "end") {
-    options <- node_options(input, current, path)
-    if (length(options) == 0) {
-      return(NULL) # found a dead end
+extend_path <- function(path, input, part = c("a", "b")) {
+  node_fun <- switch(
+    part,
+    a = node_options,
+    b = node_options_2
+  )
+
+  opts <- node_fun(input, node = path[1], path)
+  if (length(opts) == 0) { # terminal paths left untouched
+    out <- list(path)
+  } else {
+    out <- map2(.x = rep(list(path), length(opts)), .y = opts, .f = ~c(.y, .x))
+  }
+  out
+}
+
+extend_path(c("A", "start"), test_12a, part = "a")
+extend_path(c("end", "A", "start"), test_12a, part = "a")
+
+find_paths <- function(input, part = c("a", "b")) {
+  old_paths <- list("start")
+  repeat {
+    new_paths <- old_paths %>%
+      map(extend_path, input = input, part = part) %>%
+      flatten()
+
+    if (identical(new_paths, old_paths)) {
+      break
     }
-    current <- sample(options, 1)
-    path <- c(current, path) #  paths stored in reverse
+
+    old_paths <- new_paths
   }
-  path
+
+  new_paths %>%
+    keep(.p = ~.x[1] == "end")
 }
-
-find_path(test_12a)
-find_path(test_12a, "d", c("d", "b", "start"))
-find_path(test_12a, "A", c("A", "b", "start"))
-
-# For a given path and set of paths, backtrack to a node where the two could diverge
-backtrack_path <- function(input, path) {
-  new_path <- path
-  options <- character()
-  while (length(options) == 0) {
-    prev_to <- new_path[1]
-    new_path <- new_path[-1]
-    options <- node_options(input, node = new_path[1], path = new_path)
-    options <- options[options != prev_to]
-  }
-  c(sample(options, 1), new_path)
-}
-
-backtrack_path(test_12a, path = c("end", "b", "start"))
-
-find_new_path <- function(input, path, paths) {
-  new_path <- find_path(input)
-  while (has_element(paths, new_path) | is.null(new_path)) {
-    backtracked <- backtrack_path(input, new_path)
-    new_path <- find_path(input, node = new_path[1], path = new_path)
-    Sys.sleep(0.5)
-    print(new_path)
-  }
-  new_path
-}
-
-find_new_path(test_12a, path = c("end", "b", "start"), paths = list(c("end", "b", "start")))
 
 q12a <- function(input) {
-  paths <- list()
-
+  input %>%
+    find_paths(part = "a") %>%
+    length()
 }
 
-q12a(test_12a)
+q12a(test_12a) # 10
+q12a(test_12b) # 19
+q12a(test_12c) # 226
+q12a(input_12) # 3887
+
+node_options_2 <- function(input, node, path) {
+  small <- str_subset(path, "^[a-z]+$")
+  # Caves allowed if not a visited small cave, or if no cave yet visited twice
+  allowed_small <- !(input$to %in% small) | all(table(small) <= 1)
+
+  if (node == "end") {
+    out <- character()
+  } else {
+    from_node <- input$from == node
+    out <- input$to[input$from == node & allowed_small]
+  }
+  out[out != "start"] # start never allowed
+}
+
+node_options_2(test_12a, "start", "start")
+node_options_2(test_12a, "d", c("d", "b", "start"))
+node_options_2(test_12a, "A", c("A", "b", "start"))
+
+q12b <- function(input) {
+  input %>%
+    find_paths(part = "b") %>%
+    length()
+}
+
+q12b(test_12a) # 36
+q12b(test_12b) # 103
+q12b(test_12c) # 3509
+q12b(input_12) # 104834
