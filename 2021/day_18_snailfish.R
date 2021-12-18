6e0626732143f77db58ca7a35a5bd173c2e771f0
Calum You <calum@uber.com>
2021-12-18 23:08:28 +0000

    pre removal of R6 class

--- 2021/day_18_snailfish.R
+++ 2021/day_18_snailfish.R
@@ -1,64 +1,65 @@
 library(tidyverse)
+library(zeallot)
 
 SnailfishNumber <- R6::R6Class(
   classname = "SnailfishNumber",
+
   public = list(
     left = NULL,
     right = NULL,
     depth = NULL,
-    initialize = function(sfn_list) {
+    initialize = function(sfn_list, depth = 0) { # 0 depth means not nested
       stopifnot(length(sfn_list) == 2)
 
-      left <- sfn_list[[1]]
-      right <- sfn_list[[2]]
-      self$depth <- 0 # Number of contained SnailfishNumbers
-      if (is_atomic(left)) {
-        self$left <- left
-      } else {
-        self$depth <- self$depth + 1
-        self$left <- SnailfishNumber$new(left)
-      }
+      self$left <- sfn_list[[1]]
+      self$right <- sfn_list[[2]]
+      self$depth <- depth
 
-      if (is_atomic(right)) {
-        self$right <- right
-      } else {
-        self$depth <- self$depth + 1
-        self$right <- SnailfishNumber$new(right)
-      }
+      self$left <- private$atomic_if_else(
+        child = "left",
+        true = self$left,
+        false = SnailfishNumber$new(self$left, depth + 1)
+      )
+      self$right <- private$atomic_if_else(
+        child = "right",
+        true = self$right,
+        false = SnailfishNumber$new(self$right, depth + 1)
+      )
     },
-    printstr = function(...) {
-      # indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
-      c(private$leftstr(), private$rightstr())
+    to_list = function(...) {
+      left <- private$atomic_if_else("left", self$left, self$left$to_list())
+      right <- private$atomic_if_else("right", self$right, self$right$to_list())
+      pair_names <- str_c(c("L", "R"), self$depth + 1)
+      list(left, right) %>% `names<-`(pair_names)
     },
     print = function(...) {
-      cat(self$printstr(), sep = "\n")
+      str(self$to_list())
+    },
+    explode = function(...){
+      if ()
     }
   ),
 
   private = list(
-    leftstr = function(...) {
-      indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
-      if (is_atomic(self$left)) {
-        leftstr <- str_c(indent, "L:", self$left)
-      } else {
-        leftstr <- self$left$numstr()
+    atomic_if_else = function(child = c("left", "right"), true, false) {
+      if (child == "left") {
+        if (is_atomic(self$left)) {
+          return(true)
+        } else {
+          return(false)
+        }
+      } else if (child == "right") {
+        if (is_atomic(self$right)) {
+          return(true)
+        } else {
+          return(false)
+        }
       }
-      leftstr
-    },
-    rightstr = function(...) {
-      indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
-      if (is_atomic(self$right)) {
-        rightstr <- str_c(indent, "R:", self$right)
-      } else {
-        rightstr <- self$right$numstr()
-      }
-      rightstr
     }
   )
 )
-SnailfishNumber$new(list(1, 2))
 
-SnailfishNumber$new(list(list(1, 2), 3))
+SnailfishNumber$new(list(list(1, 2), 3))$to_list() %>% str()
 SnailfishNumber$new(test_18[[7]])
 
 sfn_parse <- function(raw) {
@@ -68,6 +69,7 @@ sfn_parse <- function(raw) {
     str_replace_all("\\[", "list\\(") %>%
     map(~SnailfishNumber$new(eval(parse(text = .x))))
 }
+sfn_parse("[[[[[9,8],1],2],3],4]\n")
 
 test_18 <- sfn_parse("[1,2]
 [[1,2],3]
@@ -78,6 +80,58 @@ test_18 <- sfn_parse("[1,2]
 [[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]")
 
 input_18 <- sfn_parse("2021/data/input_18.txt")
+
+sfn_explode <- function(line) {
+
+}
+
+sfn_parse <- function(raw) {
+
+}
+
+sfn_values <- function(line) {
+  level <- cumsum(c(line == "[") - c(line == "]"))
+  values_i <- str_which(line, "\\d")
+  tibble(
+    value = as.integer(line[values_i]),
+    level = level[values_i]
+  )
+}
+
+sfn_explode <- function(sfn) {
+  if (all(sfn$level <= 4)) {
+    return(sfn)
+  } else {
+    c(values, levels) %<-% sfn
+  }
+  c(lpos, rpos, ...rest = NULL) %<-% which(values > 4)
+  if (lpos > 1) values[lpos - 1] <- values[lops - 1] + values[lpos]
+  if (rpos < length(values)) values[rpos + 1] <- values[rpos + 1] + values[rpos]
+  values[rpos] <- 0
+  levels[rpos] <- levels[rpos] - 1
+  tibble(value = values[-lpos], level = levels[-lpos])
+}
+
+sfn_values(x) %>% sfn_explode()
+
+bench::mark(
+  tibble(x = letters),
+  list(x = letters),
+  check = FALSE
+)
+
+
+x <- "[[[[[9,8],1],2],3],4]" %>%
+  str_split("") %>%
+  pluck(1)
+  enframe(name = NULL, value = "char") %>%
+  filter(char != ",") %>%
+  mutate(
+    level = cumsum((char == "[") - (char == "]"))
+  ) %>%
+  filter(str_detect(char, "[0-9]"))
+
+
 
 sfn_add <- function(x, y) {
   list(x, y)
@@ -90,3 +144,33 @@ sfn_reduce <- function(x) {
 sfn_parse("[[[[[9,8],1],2],3],4]\n") %>% str()
 
 vec_depth(list(1, 2))
+
+#   printstr = function(...) {
+#     # indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
+#     c(private$leftstr(), private$rightstr())
+#   },
+#   print = function(...) {
+#     cat(self$printstr(), sep = "\n")
+#   }
+# ),
+
+# private = list(
+#   leftstr = function(...) {
+#     indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
+#     if (is_atomic(self$left)) {
+#       leftstr <- str_c(indent, "L:", self$left)
+#     } else {
+#       leftstr <- self$left$numstr()
+#     }
+#     leftstr
+#   },
+#   rightstr = function(...) {
+#     indent <- str_c(rep(" ", (4 - self$depth) * 2), collapse = "")
+#     if (is_atomic(self$right)) {
+#       rightstr <- str_c(indent, "R:", self$right)
+#     } else {
+#       rightstr <- self$right$numstr()
+#     }
+#     rightstr
+#   }
+# )
