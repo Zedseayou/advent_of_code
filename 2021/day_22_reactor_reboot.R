6e89df58c2740f639b63c0683afb8cdcbd4c12a8
Calum You <calum@uber.com>
2021-12-22 19:36:45 +0000

    give up on tracking intersection parity

--- 2021/day_22_reactor_reboot.R
+++ 2021/day_22_reactor_reboot.R
@@ -16,6 +16,7 @@ parse_input <- function(raw) {
       state = if_else(on_off == "on", 1L, 0L),
       across(c(xmin:zmax), as.numeric)
     ) %>%
+    select(-on_off) %>%
     rowwise() %>%
     group_split()
 }
@@ -27,7 +28,7 @@ input_22 <- parse_input("2021/data/input_22.txt")
 initial <- tibble(x = integer(), y = integer(), z = integer())
 
 do_step <- function(grid, instructions) {
-  c(on_off, xmin, xmax, ymin, ymax, zmin, zmax, to_state) %<-% instructions
+  c(xmin, xmax, ymin, ymax, zmin, zmax, to_state) %<-% instructions
   new_set <- crossing(x = xmin:xmax, y = ymin:ymax, z = zmin:zmax, state = to_state)
   grid %>%
     anti_join(new_set, by = c("x", "y", "z")) %>%
@@ -56,3 +57,81 @@ q22a <- function(step_list) {
 q22a(test_22a)
 q22a(test_22b[1:20])
 q22a(input_22[1:20])
+
+box_intersection <- function(box1, box2) {
+  axis_bound_pairs <- tibble(
+    min = c(box1$xmin, box2$xmin, box1$ymin, box2$ymin, box1$zmin, box2$zmin),
+    max = c(box2$xmax, box1$xmax, box2$ymax, box1$ymax, box2$zmax, box1$zmax),
+    conflict = min > max
+  )
+  if (any(axis_bound_pairs$conflict)) {
+    out <- NULL
+  } else {
+    out <- tibble_row(
+      xmin = max(box1$xmin, box2$xmin),
+      xmax = min(box1$xmax, box2$xmax),
+      ymin = max(box1$ymin, box2$ymin),
+      ymax = min(box1$ymax, box2$ymax),
+      zmin = max(box1$zmin, box2$zmin),
+      zmax = min(box1$zmax, box2$zmax),
+    )
+  }
+  out
+}
+
+box_intersection(test_22a[[1]], test_22a[[2]])
+
+box_volume <- function(box) {
+  c(xmin, xmax, ymin, ymax, zmin, zmax, state) %<-% box
+  c(xmax - xmin + 1) * c(ymax - ymin + 1) * c(zmax - zmin + 1) * state
+}
+box_volume(test_22a[[1]])
+
+# For each step, check the intersection with each lit box
+# If there is an intersection, keep the intersection with negative volume
+# If the new box turns on, keep it as well
+q22b <- function(step_list) {
+  box_list <- list()
+  overlaps <- list()
+  for (step in step_list) {
+    for (box in box_list) {
+      intersection <- box_intersection(box, step)
+      if (!is.null(intersection)) {
+        idx <- detect_index(box_list, ~ identical(.x, intersection))
+        if (idx == 0) {
+          intersection$state <- -1
+          box_list <- append(overlaps, list(intersection))
+        } else {
+          box_list[[idx]]$state <- box_list[[idx]]$state * -1
+        }
+      }
+    }
+    if (step$state == 1) {
+      box_list <- append(box_list, list(step))
+    }
+  }
+  append(box_list, overlaps) %>%
+    map_dbl(box_volume) %>%
+    sum()
+}
+
+q22b(test_22a)
+q22b(test_22b[1:20])
+
+list(
+  test_22a[[1]],
+  test_22a[[2]],
+  box_intersection(test_22a[[1]], test_22a[[2]]) %>% `$<-`(state, -1),
+  test_22a[[3]],
+  box_intersection(test_22a[[1]], test_22a[[3]]) %>% `$<-`(state, -1),
+  box_intersection(test_22a[[2]], test_22a[[3]]) %>% `$<-`(state, -1),
+  test_22a[[4]],
+  box_intersection(test_22a[[1]], test_22a[[4]]) %>% `$<-`(state, -1),
+  box_intersection(test_22a[[2]], test_22a[[4]]),
+  box_intersection(test_22a[[3]], test_22a[[4]]) %>% `$<-`(state, -1)
+) %>%
+  compact() %>%
+  map_dbl(box_volume) %>%
+  cumsum()
+
+box_intersection(test_22a[[1]], test_22a[[2]])
